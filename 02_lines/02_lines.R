a381f2604a42fee6ca6239638bbe173cdc372277
bellamendoza <141656999+bellamendoza@users.noreply.github.com>
2024-11-02 13:52:55 -0400

    02 lines

--- 02_lines/02_lines.R
+++ 02_lines/02_lines.R
@@ -1,10 +1,5 @@
 # Load the necessary libraries
-library(rnaturalearth)
-library(ggplot2)
-library(sf)
 library(dplyr)
-
-# Import Packages
 library(tigris)
 library(ggplot2)
 library(ggtext)
@@ -17,14 +12,14 @@ library(sf)
 
 # Load Google font
 font_add_google("IBM Plex Mono", "ibm-plex-mono")
-showtext_auto()  # Enable showtext
+showtext_auto()
 
 
 # Load US states shapefile
 us_states <- tigris::states(cb = TRUE)
 
-# Identify the unique states from Shakira's data (assuming there's a 'state' column)
-shakira_states <- unique(shakira_data$state)  # Get unique visited states
+# Identify the unique states from shakira data
+shakira_states <- unique(shakira_data$state)
 
 # Create a new column in the us_states data frame to indicate visited states
 us_states <- us_states %>%
@@ -37,16 +32,16 @@ color_mapping <- c("Yes" = "#ff0571", "No" = "#8f003e")
 ggplot(data = us_states) +
   geom_sf(aes(fill = visited), color = "#fff9ed", size = 0.3) +  # Fill based on visited status
   scale_fill_manual(values = color_mapping, name = "Visited", labels = c("No", "Yes")) +  # Color states
-  geom_sf(data = points_sf, aes(fill = name), shape = 21, color = "black", fill="#fc9003",  size = 2, stroke = 0.5) +  # Adjusted points
-  geom_sf(data = line_sf, color = "#fc9003", size = 1) +  # Thicker line for visibility
+  geom_sf(data = points_sf, aes(fill = name), shape = 21, color = "black", fill="#fc9003",  size = 2, stroke = 0.5) +  # Points
+  geom_sf(data = line_sf, color = "#fc9003", size = 1) +
   
-  # Label each point with dynamic repulsion and correct ordering
+  # Labeling points
   geom_label_repel(data = points_sf, aes(x = st_coordinates(geometry)[, 1],
                                          y = st_coordinates(geometry)[, 2],
                                          label = index), 
                    fill = "#fff9ed", color = "black", size = 8, max.overlaps = 12, segment.color = "#fc9003") + 
   
-  coord_sf(xlim = c(-125, -65), ylim = c(25, 50), label_axes = "----", label_graticule = "----") +  # Updated settings
+  coord_sf(xlim = c(-125, -65), ylim = c(25, 50), label_axes = "----", label_graticule = "----") +
   labs(title = "Shakira's Las Mujeres Ya No Lloran Tour", subtitle = "02 Lines | BMZA | Data source: Ticketmaster", fill = "Fruit Type") +
   theme_minimal() +
   theme(
@@ -55,14 +50,14 @@ ggplot(data = us_states) +
       margin = margin(b = 2), size=46),
     plot.subtitle = element_textbox_simple(
       family = "ibm-plex-mono", color="#8f003e",
-      padding = margin(5, 10, 10, 10),  # Adjust padding to match title
+      padding = margin(5, 10, 10, 10),
       margin = margin(t = 0, b=2),  # Avoid top margin to keep close to title
       size = 22
     ),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
-    axis.text = element_blank(),  # Remove axis labels
-    axis.ticks = element_blank(),  # Remove axis ticks
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
     plot.background = element_rect(fill = "#fff9ed", color = "#fff9ed"),
     # Legend element
     legend.background = element_rect(fill = "black", color = "black"),
